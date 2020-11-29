ffc45b29c77bfe2d770e53f1d86c6192438671c4
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-11-29 15:51:36 -0800

    No hashmap refs cause no lifetimes, also struct for index

--- src/learn/company.rs
+++ src/learn/company.rs
@@ -47,11 +47,41 @@ impl Employee {
             };
         }
     }
+    fn add_to_index(&self, index: &mut HashMap<String, String>) {
+        // Don't know how lifetimes work with references so for now we'll have to
+        // insert by value
+        // original hashmap was <&String, &Employee>
+        index.insert(self.department.clone(), self.name.clone());
+    }
 }
 
 pub fn company() {
+    let mut running = true;
+    // List all people in department sorted alphabetically
+    // List all departments sorted alphabetically
+    println!("Press ADD to add employee, DEPARTMENT to list people in department, COMPANY to list people in company, and QUIT to leave");
+    let mut input = String::new();
     // Create index of department to employee
-    let d_index:HashMap<String, Employee> = HashMap::new();
-    // First find way to get key terms from user input
-    let employee = Employee::new_from_input();
+    let mut d_index: HashMap<String, String> = HashMap::new();
+    while running {
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {}
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
+        };
+        match input {
+            "ADD" => {
+                // First find way to get key terms from user input
+                let employee = Employee::new_from_input();
+                employee.add_to_index(&mut d_index);
+            }
+            "DEPARTMENT" => {
+                
+            }
+            "COMPANY" => {}
+            "QUIT" => running = !running,
+        }
+    }
 }
